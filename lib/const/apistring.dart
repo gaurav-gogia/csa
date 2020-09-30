bbd3ed9d60ea89389bc13b0bb7805a526ac26469
Gaurav Gogia <desmondmiles36@gmail.com>
2020-09-30 23:46:24 +0530

    api strings added

---
+++ lib/const/apistring.dart
@@ -0,0 +1,6 @@
+class Api {
+  Api._();
+
+  static const STATE_DATA_API = 'https://api.covid19india.org/v4/data.json';
+  static const TIMESERIES_DATA_API = 'https://api.covid19india.org/data.json';
+}
